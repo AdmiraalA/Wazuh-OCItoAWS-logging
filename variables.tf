c06800ad72f4890e2645c5194e41621cc29e18be
Admiral <d.vanoosterhoud@gmail.com>
2024-03-19 08:24:03 +0100

    Create variables.tf
    Initial commit

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {
+  default = "us-west-1"  # Change to your desired AWS region
+}
+variable "oci_compartment_id" {}
+variable "vault_id" {}
